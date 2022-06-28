16c82ceb0566aeb0e4438146853dfac2af068ac3
monica <monicaehab16@gmail.com>
2022-06-29 01:52:57 +0200

    added homepage -not finished

--- lib/components/home.dart
+++ lib/components/home.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomeComponent extends StatelessWidget {
   final String image;
   final String text;
+  final String second;
 
-  const HomeComponent({Key? key, required this.image, required this.text})
+  const HomeComponent(
+      {Key? key, required this.image, required this.text, required this.second})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [Image.asset(image), Text(text)],
+      children: [
+        Image.asset(image),
+        Center(
+          child: Text(
+            text,
+            style: const TextStyle(
+                color: Colors.black, fontSize: 25, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Center(
+          child: Text(
+            second,
+            style: const TextStyle(
+                color: Colors.grey, fontSize: 15, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
     );
   }
 }
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:task1/components/home.dart';
 
+List<String> mainParagraph = [
+  'Get food delivery to your doorstop asap',
+  'Buy Any Food from your favourite restaurant'
+];
+List<String> secParagraph = [
+  'we have young and professional delivery team that will bring your food as soon as possible to your doorstep',
+  'we are constantly adding your favourite restaurant throughout the territory and around your area carefully selected'
+];
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
@@ -13,10 +22,10 @@ class _MyHomePageState extends State<MyHomePage> {
   final PageController pageController = PageController();
 
   updatePageState(index) {
-    if (index == 1)
+    if (index == 1) {
       pageController.nextPage(
           duration: Duration(milliseconds: 300), curve: Curves.easeIn);
-
+    }
     setState(() {
       _currentIndex = index;
     });
@@ -26,9 +35,9 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-            padding: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(20),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              // mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 Row(
                   mainAxisAlignment: MainAxisAlignment.end,
@@ -40,21 +49,31 @@ class _MyHomePageState extends State<MyHomePage> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: const [Text('7krave')],
                 ),
-                Image.asset('assets/images/bike1.png'),
-                PageView(
-                    controller: pageController,
-                    onPageChanged: (num) {
-                      setState(() {
-                        _currentIndex = num;
-                      });
-                    },
-                    scrollDirection: Axis.horizontal,
-                    children: const [
-                      HomeComponent(
-                          image: 'assets/images/bike1.png', text: 'hellooo1'),
-                      HomeComponent(
-                          image: 'assets/images/bike1.png', text: 'hellooo2')
-                    ])
+                // Image.asset('assets/images/bike1.png'),
+                SizedBox(
+                  height: 700,
+                  child: PageView(
+                      controller: pageController,
+                      onPageChanged: (num) {
+                        updatePageState(num);
+                        setState(() {
+                          _currentIndex = num;
+                        });
+                      },
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        HomeComponent(
+                          image: 'assets/images/bike1.png',
+                          text: mainParagraph[0],
+                          second: secParagraph[0],
+                        ),
+                        HomeComponent(
+                          image: 'assets/images/bike1.png',
+                          text: mainParagraph[1],
+                          second: secParagraph[1],
+                        )
+                      ]),
+                )
               ],
             )));
   }
